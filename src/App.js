d02a595fbf52f61b33e7902b5269d6334ecf3eac
Juan  Gomez <jcgom3@gmail.com>
2022-04-06 20:31:11 -0400

    comment out movie const

--- src/App.js
+++ src/App.js
@@ -13,8 +13,7 @@ const API_URL = "http://www.omdbapi.com?apikey=c1be11fa";
 //   Type: "movie",
 //   Poster:
 //     "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
-//  };
-//  vvvv
+// };
 
 const App = () => {
   const [movies, setMovies] = useState([]);
@@ -25,7 +24,7 @@ const App = () => {
     setMovies(data.Search);
   };
   useEffect(() => {
-    searchMovies("Avengers");
+    searchMovies("Batman");
   }, []);
 
   return (

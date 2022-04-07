cf5cd78737177fac1a513d4dc3c399a9b2293ebe
Juan  Gomez <jcgom3@gmail.com>
2022-04-06 23:13:53 -0400

    cleaned up

--- src/App.js
+++ src/App.js
@@ -1,44 +1,45 @@
 import { useEffect, useState } from "react";
 import MovieCard from "./MovieCard";
-import "./App.css";
 import SearchIcon from "./search.svg";
+import "./App.css";
 
 const API_URL = "http://www.omdbapi.com?apikey=c1be11fa";
 
-const movie = {
-  Title: "Avengers: Infinity War",
-  Year: "2018",
-  imdbID: "tt4154756",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
-}
+// const movie = {
+//   Title: "Avengers: Infinity War",
+//   Year: "2018",
+//   imdbID: "tt4154756",
+//   Type: "movie",
+//   Poster:
+//     "https://m.media-amazon.com/images/M/MV5BMjMxNjY2MDU1OV5BMl5BanBnXkFtZTgwNzY1MTUwNTM@._V1_SX300.jpg",
+// }
 
 const App = () => {
-
-  const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
+  const [movies, setMovies] = useState([]);
+
+  useEffect((movie) => {
+    searchMovies("Batman");
+  }, []);
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
     const data = await response.json();
     setMovies(data.Search);
   };
-  useEffect((movie) => {
-    searchMovies("Batman");
-  }, []);
 
   return (
     <div className="app">
       <h1>MovieLand</h1>
+
       <div className="search">
         <input
-          placeholder="Search for Movies"
           value={searchTerm}
+          placeholder="Search for Movies"
           //e.target.value on the setSearchTerm to be able to type in the input search bar
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        {/* dynamically render the the array of movies + images from the new search */}
+        {/* /* dynamically render the the array of movies + images from the new search */}
         <img
           src={SearchIcon}
           alt="search"
